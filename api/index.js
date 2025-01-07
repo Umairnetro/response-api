4727476b2c530b9ed78ec3d7a41c68827548965d
Umair Ahmed <umairjdu@gmail.com>
2025-01-07 17:22:29 +0500

    add new end point

--- api/index.js
+++ api/index.js
@@ -1,3 +1,30 @@
-const app = require('../index');
+const express = require("express");
+const users = require("../data/users");
 
-module.exports = app
+const app = express();
+const router = express.Router();
+
+app.use(express.json());
+
+router.get("/", (req, res) => {
+  res.send({
+    message: "response From Server...",
+    method: req.method,
+    url: req.url,
+    goTo: "http://localhost:3010/api/users",
+  });
+});
+
+router.get("/users", (req,res)=>{
+  res.send({
+    users
+  })
+})
+
+app.use(router);
+
+app.listen(3010, () => {
+  console.log("server is running on port 3010");
+});
+
+module.exports = app;
---
+++ data/users.js
@@ -0,0 +1,27 @@
+// Sample data array
+const users = [
+    {
+      id: 1,
+      name: "John Doe",
+      email: "john.doe@example.com",
+      age: 30,
+      isActive: true,
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      email: "jane.smith@example.com",
+      age: 25,
+      isActive: false,
+    },
+    {
+      id: 3,
+      name: "Alice Johnson",
+      email: "alice.johnson@example.com",
+      age: 35,
+      isActive: true,
+    },
+  ];
+  
+  module.exports = users;
+  
